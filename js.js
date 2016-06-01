adefb25f0bd424492fb4665a777f3b3d900b8952
Lyuba Leskova <l_buzya@ukr.net>
2016-06-01 06:52:50 +0300

    Stage all files

---
+++ js.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    $("#menu").click(function () {
+        $("nav.main").toggle();
+    })
+
+    var anchor = $("#top");
+    $(window).on("scroll", showAnchor)
+    
+function showAnchor () {
+    var top = $("body").scrollTop();
+    if (top > 700) anchor.fadeIn(1000);
+    else anchor.fadeOut(1000);
+}
+
+$(".jewelery img, .photoblog img").click(function () { // добавити стрілки вліво вправоб закрити
+        $("#shadow").fadeIn();
+        var src = $(this).attr("src");
+        $("#shadow img").attr("src", src);
+    })
+})
